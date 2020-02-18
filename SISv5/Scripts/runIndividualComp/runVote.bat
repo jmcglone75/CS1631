7724916fa3cf610645cf1f22db95ff83f38e60fd
Jake McGlone <jmcglone75@gmail.com>
2020-02-18 08:56:41 -0800

    milestone 1

---
+++ SISv5/Scripts/runIndividualComp/runVote.bat
@@ -0,0 +1,6 @@
+@echo off
+title Vote
+
+javac -sourcepath ../../Components/Vote -cp ../../Components/* ../../Components/Vote/*.java
+start "Vote" /D"../../Components/Vote" java -cp .;../* CreateVote
+PAUSE
